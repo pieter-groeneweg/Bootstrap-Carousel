86a6d4bc693dbbd41d367fc60b4a00b78f5ec039
pieter-groeneweg <pieter@groenewegdigitaal.nl>
2020-09-03 13:56:05 +0200

    Update carousel.php

--- carousel.php
+++ carousel.php
@@ -93,10 +93,10 @@ $count = min(ceil($params->get('count', 5)/$numsfx), ceil(count($list)/$numsfx))
     <!-- Left and right controls -->
     <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
       <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <!--<span class="sr-only">Previous</span>-->
+      <!--as it looks crappy without styling this is commented out<span class="sr-only">Previous</span>-->
     </a>
     <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
       <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <!--<span class="sr-only">Next</span>-->
+      <!--as it looks crappy without styling this is commented out<span class="sr-only">Next</span>-->
     </a>    
 </div>
