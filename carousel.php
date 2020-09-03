81e18f361bc11fcf04afb0c36a57461a5d8f3485
pieter-groeneweg <pieter@groenewegdigitaal.nl>
2020-09-03 22:00:46 +0200

    Update carousel.php
    cleaned obsolete code and reduced variables.
    changed controls to joomla icomoon arrows.

--- carousel.php
+++ carousel.php
@@ -6,64 +6,46 @@
  * drop this in <template>)/html/mod_articles_latest
  * beware this is bootstrap 3, make sure your j! site has bootstrap 3 enabled
  * enjoy making changes if you need. 
- 
+ * 
+ * Copyright 2020 - pieter groeneweg - info@bizgo.nl
  */
 
 defined('_JEXEC') or die;
-
 //number of items in a slide is set in module class suffix
 $numsfx = max($params->get('moduleclass_sfx'),1); //needs improvement when ading more than one sfx. (ISOLATE the number)
-
 //real item (slide) count can be less than set in count parameter
 $count = min(ceil($params->get('count', 5)/$numsfx), ceil(count($list)/$numsfx));
-
 ?>
-
-
-
-
-<div id="myCarousel<?php echo $module->id; ?>" class="<?php echo $count; ?> carousel slide" data-ride="carousel" data-interval="5500">
+<div id="myCarousel<?php echo $module->id; ?>" class="carousel slide" data-ride="carousel" data-interval="5500">
     <!-- Indicators -->
     <ol class="carousel-indicators">
-	
 	<?php
-		//adding the indicators by count of articles in the parameters
+		//adding the indicators (dsl) by count of articles in the parameters
 		for ($dsl = 0; $dsl < $count; $dsl++) {
-        
-        	//set the active state on the first on html render
+        	//set the active class (cla) on the first on html render
 			if ($dsl == 0) {
 				$cla = "class='active'";
 			} else {
 				$cla = "";
 			}
-        
 			echo "<li data-target='#myCarousel".$module->id."' data-slide-to='".$dsl."' ".$cla."></li> \n";
         }
 	?>
-
-    </ol>
-
+	</ol>
     <!-- Wrapper for slides -->
-  
- 	<div class="carousel-inner" role="listbox">
-  
-	<?php 
-    	$lit = 0; //set the list item number
-    	
-    	$sld = 0; //$sld = $numsfx;	//set the number of items per slide - a countdown will determine when new series of items per slide starts.
-    
-    	foreach ($list as $item) {
-		
-        //set the active state on the first on html render
+   	<div class="carousel-inner" role="listbox">
+  	<?php 
+    	$lit = 0; //set the list item (lit) number
+      	foreach ($list as $item) {
+        //set the active class (cla) on the first on html render
   		if ($lit == 0) {
-        	$cli = " active";
+        	$cla = " active";
         } else {
-        	$cli = "";
+        	$cla = "";
         }
-        
         //get the slides to start before each series of articles
-        if($sld % $numsfx == 0) {
-			echo "<div class='".$sld." ".$lit/$numsfx." item".$cli."'> \n";
+        if($lit % $numsfx == 0) {
+			echo "<div class='item".$cla."'> \n";
         }
         //have the articles spread over the slide
 		echo "<div style='width:". (100/$numsfx) . "%; float:left; position: relative;'> \n";
@@ -77,26 +59,22 @@ $count = min(ceil($params->get('count', 5)/$numsfx), ceil(count($list)/$numsfx))
         echo "</a> \n";
         //end part at will
         
-        
         echo "</div> \n";
-        
+        $lit++;
         //get the slides to end after each series of articles
-        $sld++; //countdown
-        if($sld % $numsfx == 0) {
+        if($lit % $numsfx == 0) {
         	echo "</div> \n";
         }
-		$lit++;
 	}
-  
   	?>
-</div>
+	</div>
     <!-- Left and right controls -->
     <a class="left carousel-control" href="#myCarousel<?php echo $module->id; ?>" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <!--<span class="sr-only">Previous</span>-->
+      <span class="icon icon-arrow-left-3" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
     </a>
     <a class="right carousel-control" href="#myCarousel<?php echo $module->id; ?>" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <!--<span class="sr-only">Next</span>-->
+      <span class="icon icon-arrow-right-3" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
     </a>    
 </div>
